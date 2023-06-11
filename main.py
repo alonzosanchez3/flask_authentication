fd8c0f742cc56949328458fc46285d56b770acb9
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-06-11 16:34:23 -0500

    now storing users passwords hashed and salted

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
+        hashedPassword = generate_password_hash(request.form.get('password'), method='pbkdf2:sha256', salt_length=8)
         new_user = User(
             email = request.form.get('email'),
-            password = request.form.get('password'),
+            password = hashedPassword,
             name = request.form.get('name')
         )
         db.session.add(new_user)
